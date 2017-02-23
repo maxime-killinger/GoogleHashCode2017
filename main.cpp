7a9f9e81ac0d5f58f3bbd5d31a110b6888492f44
Maxime Killinger <max.killinger@outlook.fr>
2017-02-23 20:56:30 +0100

    Make functional main

--- main.cpp
+++ main.cpp
@@ -5,19 +5,19 @@
 using namespace std;
 
 int main() {
-    ifstream                    file("GoogleFiles/kittens.in", ios::in);
+    ifstream                    file("../GoogleFiles/kittens.in", ios::in);
     string                      buff;
-    istringstream      iss(buff);
     string             token;
 
     if (!file) {
         cout << "Error opening file" << endl;
         return (-1);
     }
-    getline(file, buff);
-    while (getline(iss, token, ' '))
-    {
-        cout << token << endl;
+    while (getline(file, buff)) {
+        istringstream      iss(buff);
+        while (getline(iss, token, ' ')) {
+            cout << token << endl;
+        }
     }
     file.close();
     return (0);
